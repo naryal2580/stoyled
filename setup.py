bffa0d073e47d736a938ae2f36bf50d70d391b53
naryal2580 <naryal2580@gmail.com>
2020-02-24 09:27:23 +0545

    included pypi stuff

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("./README.md") as f:
+    long_description = f.read()
+
+setup(
+    name='stoyled',
+    version='0.1',
+    author="Captain Nick Lucifer",
+    author_email="naryal2580@gmail.com",
+    url="https://github.com/naryal2580/stoyled",
+    download_url='https://github.com/naryal2580/stoyled/tarball/master',
+    description="Simple Fancy Standard Outputs for python3",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    py_modules=['stoyled'],
+    data_files=[('', ['LICENSE'])],
+    include_package_data=True,
+    classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+    python_requires='>=3',
+ )
