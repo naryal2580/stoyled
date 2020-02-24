c71c85849203ceefdff46519ef25a93e27d6ec98
naryal2580 <naryal2580@gmail.com>
2020-02-24 19:12:41 +0545

    fixed, text bug; version++; misc

--- stoyled/__init__.py
+++ stoyled/__init__.py
@@ -1,1 +1,5 @@
 from .nGenStyls import *
+
+
+__version__ = "0.2"
+__author__ = "naryal2580"
--- stoyled/genStyls.py
+++ stoyled/genStyls.py
@@ -31,7 +31,7 @@ formats = {
 
 
 def retnFunc(_format):
-    func = "def "+_format+"(text,color=True):\n\ttext=str(text)\n\tif len(text.split(\' -> \'))==2:\n\t\ttext=text.split(\' -> \')\n\tif color:\n\t\tif type(text)==str:\n\t\t\treturn f\'{rst}[{rst+bold+"+formats[_format][1]+"}"+formats[_format][0]+"{rst+white}] {rst+"+formats[_format][2]+"}{text}{rst}\'\n\t\telif type(text) == list:\n\t\t\treturn f\'{rst+white}[{rst+bold+"+formats[_format][1]+"}"+formats[_format][0]+"{rst+white}]{rst+"+formats[_format][2]+"} {text[0]}: {rst+"+formats[_format][1]+"+italic}{text[1]}{rst}\'\n\telse:\n\t\tif type(text)==str:\n\t\t\treturn f\'["+formats[_format][0]+"] {text}\'\n\t\telif type(text)==list:\n\t\t\treturn \'["+formats[_format][0]+"] {text[0]}: {text[1]}\'"
+    func = "def "+_format+"(text,color=True):\n\ttext=str(text)\n\tif len(text.split(\' -> \'))==2:\n\t\ttext=text.split(\' -> \')\n\tif color:\n\t\tif type(text)==str:\n\t\t\treturn f\'{rst}[{rst+bold+"+formats[_format][1]+"}"+formats[_format][0]+"{rst+white}] {rst+"+formats[_format][2]+"}{text}{rst}\'\n\t\telif type(text) == list:\n\t\t\treturn f\'{rst+white}[{rst+bold+"+formats[_format][1]+"}"+formats[_format][0]+"{rst+white}]{rst+"+formats[_format][2]+"} {text[0]}: {rst+"+formats[_format][1]+"+italic}{text[1]}{rst}\'\n\telse:\n\t\tif type(text)==str:\n\t\t\treturn f\'["+formats[_format][0]+"] {text}\'\n\t\telif type(text)==list:\n\t\t\treturn f\'["+formats[_format][0]+"] {text[0]}: {text[1]}\'"
     return func
 
 
