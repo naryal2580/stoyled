d33663f5dd480e8fe595452cd3e26c096323a08c
Captain Nick Lucifer <naryal2580@gmail.com>
2020-04-23 04:37:55 +0545

    quick lazy lint fix

--- stoyled/__init__.py
+++ stoyled/__init__.py
@@ -1,5 +1,15 @@
+from .nGenStyls import info, \
+                       good, \
+                       warn, \
+                       bad, \
+                       coolInput, \
+                       takenInput, \
+                       coolExit, \
+                       fetchFormatedTime
+
+
 from .nGenStyls import *
 
 
-__version__ = "0.4"
+__version__ = "0.5"
 __author__ = "naryal2580"
