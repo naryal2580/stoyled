9e96a327cd4a44df3a76b1eaefdfe413308e771c
naryal2580 <naryal2580@gmail.com>
2020-06-15 20:17:50 +0545

    misc changes

--- stoyled/__init__.py
+++ stoyled/__init__.py
@@ -1,3 +1,8 @@
+"""
+Stoyled
+
+Standard Output Styled
+"""
 from .moreStyles import info, \
                         good, \
                         warn, \
@@ -33,11 +38,13 @@ from .moreStyles import info, \
 
 from os import name
 
+# Adding ANSI Escape Sequence support to Windows devices, check will be made better after I'll enum all reutrn of `os.name`
 if name != 'posix':
     from ctypes import windll
     windll.kernel32.SetConsoleMode(windll.kernel32.GetStdHandle(-11), 7)
+    del windll
 
 __version__ = "0.8"
 __author__ = "naryal2580"
 
-del generateStyles, moreStyles, escapeSequences
+del generateStyles, moreStyles, escapeSequences, name
